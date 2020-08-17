1e5257a49676c93d1bb40d31eea3e65008ef9fae
humulla <humairaa.mulla@hackney.gov.uk>
2020-08-17 11:09:47 +0100

    Changes to isNullOrWhiteSpace()

--- ResidentInformationApi/V1/Gateways/Helpers/DictionaryBuilder.cs
+++ ResidentInformationApi/V1/Gateways/Helpers/DictionaryBuilder.cs
@@ -10,10 +10,10 @@ namespace ResidentInformationApi.V1.Gateways.Helpers
         {
             var queryDictionary = new Dictionary<string, string>();
 
-            if (!string.IsNullOrEmpty(rqp.FirstName)) queryDictionary.Add("first_name", rqp.FirstName);
-            if (!string.IsNullOrEmpty(rqp.LastName)) queryDictionary.Add("last_name", rqp.LastName);
-            if (!string.IsNullOrEmpty(rqp.Address)) queryDictionary.Add("address", rqp.Address);
-            if (!string.IsNullOrEmpty(rqp.Postcode)) queryDictionary.Add("postcode", rqp.Postcode);
+            if (!string.IsNullOrWhiteSpace(rqp.FirstName)) queryDictionary.Add("first_name", rqp.FirstName);
+            if (!string.IsNullOrWhiteSpace(rqp.LastName)) queryDictionary.Add("last_name", rqp.LastName);
+            if (!string.IsNullOrWhiteSpace(rqp.Address)) queryDictionary.Add("address", rqp.Address);
+            if (!string.IsNullOrWhiteSpace(rqp.Postcode)) queryDictionary.Add("postcode", rqp.Postcode);
 
             var rqpString = new FormUrlEncodedContent(queryDictionary).ReadAsStringAsync().Result;
 
